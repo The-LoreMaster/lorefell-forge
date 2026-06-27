5ae4bb5e28b6db24cbce5c6741204665fc280834
LoreKeeper <lorekeeper@local>
2026-06-27 02:03:13 +0000

    FellGlass bridge: relay declared damage (dmg) to backend

--- velo/page-fellglass.js
+++ velo/page-fellglass.js
@@ -73,7 +73,7 @@ $w.onReady(() => {
     } else if (msg.type === 'combat-declare') {
       try {
         await saveCombatDeclare(msg.charId || charId, {
-          act: msg.act, react: msg.react, target: msg.target, round: msg.round,
+          act: msg.act, react: msg.react, target: msg.target, round: msg.round, dmg: msg.dmg,
           charge: msg.charge, curVit: msg.curVit, maxVit: msg.maxVit, affs: msg.affs
         });
       } catch (e) {}
